27518037d707bebe776007f9b517ad1a108079fe
tugi <blueqwinn@gmail.com>
2023-02-08 15:22:21 +0300

    third commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,32 @@
 class Dog
 
+    # setter method
+    def name =(name)
+        @name = name
+    end
+
+    # getter method 
+
+    def name 
+        @name
+    end
+end
+
+def  breed
+    @Mastiff.breed
+
+    @chihuahua .breed
+
+   @Corgi.breed
+
+    @Shar Pei .breed
+
+    @Beagle.breed
+   
+    @French Bulldog.breed
+
+    @Pug.breed
+
+    @pointer.breed
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+ 
+    # setter method
+def name=(name)
 
+    @name = name
 end
+
+# getter method
+
+def name
+
+    @name
+   end
+end
+
+kanye = Person.new
+kanye.name = "kanye"
+
+kenya.name #=> "Kanye"
